ca8e56c8b1caba732fea67422fee13ef48878aea
AgostaGiorgio <agogi.dev@gmail.com>
2025-10-29 22:38:48 +0100

    add logic to create db, set table and manage CRUD

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from src.config.logger import *
 from src.utils.health_server import start_health_server
 
@@ -6,13 +7,24 @@ from src.di import DIContainer
 logger = logging.getLogger(__name__)
 
 
-def main():
-    containter = DIContainer()
-    bot = containter.mtrack_bot()
-    
+async def main():
     start_health_server(port=8080)
-    
-    bot.run()
+
+    containter = DIContainer()
+    db_manager = containter.db_manager()
+    bot = containter.mtrack_bot()
+
+    await db_manager.initialize()
+    await bot.run()
+
+    try:
+        await asyncio.Event().wait()
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Stopping bot...")
+    finally:
+        await bot.stop()
+        await db_manager.close()
+        logger.info("Bot stopped!")
     
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
