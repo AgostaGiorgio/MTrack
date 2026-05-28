59bab71f7638fff000817bc71693ff0ef7441afb
AgostaGiorgio <agogi.dev@gmail.com>
2026-05-28 11:59:39 +0400

    feat: remove comment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css' // <-- Aggiunto per caricare Tailwind
+import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
